9ac11d28c86d03b62d26dac3cf4204d9c1a40d82
MD <dhmedy@googlemail.com>
2022-08-16 15:32:09 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+let
+    numArr = [5, 22, 15, 100, 55]
+
+
+/*
+Ein Array aus Zahlen und möchten herausfinden, durch welche Zahlen sie (außer durch eins und sich selbst) teilbar sind.
+*/
+
+let arrFnArrayIterator = (numArr) => {
+
+    document.write('<h1>Please open the console! : </h1>');
+
+    document.write(`<p>Array numArr = ${numArr} </p>`);
+    document.write('<hr>');
+    for (let index = 0; index < numArr.length; index++) {
+        arrFnZahlen(numArr[index]);
+    }
+}
+
+let arrFnZahlen = (nr) => {
+    for (let i = 2; i < nr; i++) {
+        if (nr % i === 0) {
+            console.log('Die Zahl ' + nr + ' lässt sich durch ' + i + ' teilen! ');
+            console.log(' Das Ergebnis von (' + nr + ' / ' + i + ') = ' + (nr / i));
+            document.write('<p> Die Zahl ' + nr + ' lässt sich durch ' + i + ' teilen! ' + ` Das Ergebnis von (' + ${nr} + ' / ' + ${i} + ') = ${(nr / i)} </p>`);
+
+
+        }
+    }
+}
+
+arrFnArrayIterator(numArr);
